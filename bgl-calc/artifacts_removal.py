2dbb3757a28817d4a9c4e2f46fa568d60b72371f
Saad Qureshi <msaadq94@gmail.com>
2017-04-12 00:18:37 +0500

    Adds the training datasets and starter code for adaptive filter.

--- bgl-calc/artifacts_removal.py
+++ bgl-calc/artifacts_removal.py
@@ -1,5 +1,19 @@
-import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
 
-a = np.array([1, 2, 3])
+training_data = sio.loadmat("ppg-data/training-data/DATA_01_TYPE01.mat")
 
-print a
+ppg1 = training_data['sig'][1]
+ppg2 = training_data['sig'][2]
+
+acc_x = training_data['sig'][3]
+acc_y = training_data['sig'][4]
+acc_z = training_data['sig'][5]
+
+plt.figure()
+plt.plot(ppg1[0:400])
+
+plt.figure()
+plt.plot(ppg2[0:400])
+
+plt.show()
